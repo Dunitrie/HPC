ad64346a5169bb4eee1ee1ff71f7f41b190193aa
Dun <danielburkhardt@freenet.de>
2023-07-26 16:51:01 +0200

    added communications in every direction

--- main.py
+++ main.py
@@ -20,10 +20,19 @@ n_blocks = number_of_blocks((nx_total, ny_total), size)
 # Initialize local grid parameters:
 # local size
 nx, ny = width_height(rank, nx_total, ny_total, n_blocks)
-boundaries = bool_boundaries(rank, nx, ny, n_blocks)
-
+borders = bool_boundaries(rank, nx, ny, n_blocks)
+# rank of the process, where there's no (bounce-back-)boundary
+if borders[0]:
+    rank_right = rank + 1
+elif borders[2]:
+    rank_left = rank - 1
+if borders[1]:
+    rank_up = rank - n_blocks[1]
+elif borders[3]:
+    rank_down = rank + n_blocks[1]
+    
 # Initialize grid
-grid = np.zeros(9, nx+2, ny+2)
+f = np.zeros(9, nx+2, ny+2)
 ... # initialize beginning distribution
 
 # loop over timesteps
@@ -33,13 +42,31 @@ for idx_time in ...:
     # 2. do the walls:
     # We should probably distinguish between the indices
     for idx in range(4):
-        if boundaries[idx]:
-    
+        if borders[idx]:
+            
     
     # 3. do the communications
+    if not borders[0]:
+        if not borders[2]:
+            comm.Sendrecv(f[:, :, -2], rank_right, recvbuf=f[:, :, 0], source = rank_left)
         else:
-            comm.Sendrecv(...)
-    
+            comm.Send(f[:, :, -2], rank_right)
+    if not borders[1]:
+        if not borders[3]:
+            comm.Sendrecv(f[:, 1, :], rank_up, recvbuf=f[:, -1, :], source=rank_down)
+        else:
+            comm.Sendrecv(f[:, 1, :], rank_up)
+    if not borders[2]:
+        if not borders[0]:
+            comm.Sendrecv(f[:, 1, :], rank_left, recvbuf=f[:, -1, :], source = rank_right)
+        else:
+            comm.Send(f[:, 1, :], rank_left)
+    if not borders[3]:
+        if not borders[1]:
+            comm.Sendrecv(f[:, -2, :], rank_down, f[:,0,:], source = rank_up)
+        else:
+            comm.Send(f[:, -2, :], rank_down)
+                
     # 4. do the plotting
     if idx_time % (n_timesteps // n_plots) == 0:
         # stack everything in rank 0
