12f2714e134f9c1f39d1c0f08ceb8f1fa61e6050
Dun <danielburkhardt@freenet.de>
2023-08-01 16:48:41 +0200

    REAL plotting should be working?

--- plotting_functions.py
+++ plotting_functions.py
@@ -1,18 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_velocity(f, v, c, return_plot=False, fix_dims=False):
+def plot_velocity(f, c=None, v=None, return_plot=False, fix_dims=False):
     """ Plot the averagae velocity of the distribution f.
+    
     
     Args:
         f: probability distribution
         
-        v: velocity for each channel if already computed so we avoid computing it again. Otherwise computed as sum over channels times c
+    Optional:
         c: Directions of D2Q9-scheme
+        v: velocity for each channel if already computed so we avoid computing it again. Otherwise computed as sum over channels times c
         return_plot: If True, return the axis-Object to plot it or change it in other file
-            Optional:
-
         fix_dims: If True: To keep plots coherent, invert x-axis and change x and y-axis
+    
     """
     
     if c is None:
@@ -21,34 +22,33 @@ def plot_velocity(f, v, c, return_plot=False, fix_dims=False):
         c = np.stack([c[:,1], -c[:,0]], axis=1) 
     if v is None:
         v = np.einsum('cij, cd -> dij', f, c)
-        
+    v = np.swapaxes(v, 1,2)
+    #f = np.swapaxes(f, 1,2)
         
     ax = plt.subplot()
-    x, y = np.meshgrid(np.arange(f.shape[1]), np.arange(f.shape[2]))
-    ax.quiver(x,y, v[0,...], v[1,...], angles='xy', scale_units='xy', scale=1, color='b', label='Vector Field')
+    y, x = np.meshgrid(np.arange(f.shape[1]), np.arange(f.shape[2]))
+    ax.quiver(x,y, v[1,...], v[0,...], angles='xy', scale_units='xy', scale=1, color='b', label='Vector Field')
     ax.grid()
-    ax.set_xlabel('X')
-    ax.set_ylabel('Y')
-    ax.set_xticks([])
-    ax.set_yticks([])
+    ax.set_xlabel('Y')
+    ax.set_ylabel('X')
     ax.axis('equal')
-    ax.set_xlim(-2, f.shape[1]+1)
-    ax.set_ylim(-2, f.shape[2]+1)
+    ax.set_xlim(-2, f.shape[2]+1)
+    ax.set_ylim(-2, f.shape[1]+1)
     ax.set_title("Velocity field")
+    ax.invert_yaxis()
     
     
     #plot the edges
-    ax.plot(np.arange(-1,x.shape[1]+1), np.ones(x.shape[1]+2)*(-1), 'k')
-    ax.plot(np.arange(-1,x.shape[1]+1), np.ones(x.shape[1]+2)*(x.shape[0]), 'k')
+    plt.plot(np.arange(-1,x.shape[0]+1), np.ones(x.shape[0]+2)*(-1), 'k')
+    plt.plot(np.arange(-1,x.shape[0]+1), np.ones(x.shape[0]+2)*(x.shape[1]), 'k')
     
-    ax.plot(np.ones(x.shape[0]+2)*(-1), np.arange(-1,x.shape[0]+1), 'k')
-    ax.plot(np.ones(x.shape[0]+2)*(x.shape[1]), np.arange(-1,x.shape[0]+1), 'k')
+    plt.plot(np.ones(x.shape[1]+2)*(-1), np.arange(-1,x.shape[1]+1), 'k')
+    plt.plot(np.ones(x.shape[1]+2)*(x.shape[0]), np.arange(-1,x.shape[1]+1), 'k')
     
     if return_plot:
         return ax
     else:
         plt.show()
-        
         
 def plot_velocity_slice(f, c=None, v=None, return_plot=False, fix_dims=True, avg_vel=True):
     """ Plot the averagae velocity of the distribution f over a slice to the right.
