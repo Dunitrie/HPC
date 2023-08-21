a3350341f19669cdf94794e958abc42f6c88bb2d
friedseb <132606429+friedseb@users.noreply.github.com>
2023-08-21 11:43:33 +0200

    streamplot code and moving wall speed depending on Re

--- scenarios.py
+++ scenarios.py
@@ -7,8 +7,8 @@ from plotting_functions import plot_velocity, plot_velocity_slice
 # Possibilities: "shear wave 1", "shear wave 2", "couette flow", "poiseuille flow", "sliding lid"
 scenario = "sliding lid"
 
-n_timesteps = 51
-n_plots = 5
+n_timesteps = 1001
+n_plots = 2
 
 # Initialize Grid:
 nx = 30  # num of rows
@@ -18,7 +18,7 @@ ny = 20  # num of columns
 weights = np.array([4/9, 1/9, 1/9, 1/9, 1/9, 1/36, 1/36, 1/36, 1/36])
 c = np.array([[0, 0], [0, 1], [-1, 0], [0, -1], [1, 0], [-1, 1], [-1, -1], [1, -1], [1, 1]])
 
-wall_speed_north = 0
+wall_speed_north = 1
 
 match scenario:
     case "shear wave 1":
@@ -32,7 +32,15 @@ match scenario:
         borders = [1, 1, 1, 1]
     case "sliding lid":
         borders = [1, 1, 1, 1]
-        wall_speed_north = 0.1
+        # Reynolds number calculation: Leaving system lenght L = 300 and viscosity of water nhy= 10^-6 m^2/s,
+        # modifying the velocity of the moving wall
+        # return the velocity of the moving wall with constant Re
+        L = ny
+        nü = 10**(-6)
+        Re = 1000
+        def comp_vel_moving_wall(Re, L):
+            return (Re * nü) / L
+         wall_speed_north = comp_vel_moving_wall(Re, L)
 
 match scenario:
     case "shear wave 1":
@@ -60,11 +68,17 @@ for idx_time in range(n_timesteps):
         print(idx_time)
 
         #plot_velocity(f, v, return_plot=True)
-        plot_velocity(f[:, 1:-1, 1:-1], v[:, 1:-1, 1:-1], return_plot=True)
-
+        plot_velocity(f[:, 1:-1, 1:-1], v[:, 1:-1, 1:-1], return_plot=False)
+        plt.savefig("slidinglid.png")
         plt.show()
 
     # Calculate the streaming step wrt (global) boundary conditions
     f, rho, v = streaming(f, rho, v, c, weights, borders, wall_speed_north, scenario)
 
     rho, v = recalculate_functions(f, rho, v, c)  # Update values
+
+# streamplot for velocity field at end of simulation
+Y, X = np.mgrid[0:nx, 0:ny]
+plt.streamplot(X, Y, v[0,1:nx+1, 1:ny+1], v[1,1:nx+1, 1:ny+1 ], density=[1,1])
+plt.savefig("slidinglidstream.png")
+plt.show()
